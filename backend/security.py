c1c10e17be2d3ebb4177ad3772481fb2d65eb5b0
Satyavardhan Koyalkar <satyavardhan24105@gmail.com>
2026-03-05 20:24:18 +0530

    fix bcrypt render issue

--- backend/security.py
+++ backend/security.py
@@ -1,26 +1,25 @@
 from passlib.context import CryptContext
-
-from jose import jwt
+from jose import jwt, JWTError
 from datetime import datetime, timedelta
-from config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
-from jose import JWTError
 from fastapi import HTTPException
-from config import SECRET_KEY, ALGORITHM
+from config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt_sha256"], deprecated="auto")
+
 
 def hash_password(password: str):
     return pwd_context.hash(password)
 
+
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
 
 def create_access_token(data: dict):
     to_encode = data.copy()
 
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
@@ -28,10 +27,7 @@ def create_access_token(data: dict):
     return encoded_jwt
 
 
-
-
 def verify_token(token: str):
-
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 
